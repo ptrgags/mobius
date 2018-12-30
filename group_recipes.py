4a85cefd83d0330bf8958528c0995e9205cf495e
Peter Gagliardi <pcg34@drexel.edu>
2018-12-30 16:27:23 -0500

    Add Grandma's Recipe

--- group_recipes.py
+++ group_recipes.py
@@ -5,9 +5,75 @@ These return lists of transformations including inverses.
 Most of these are from Indra's Pearls by David Mumford et al.
 """
 from mobius import Mobius
+import cmath
 
 def make_group(*xforms):
+    """
+    Add inverses to a list of transformations
+    """
     return list(xforms) + [xform.inv for xform in xforms]
+
+def solve_quadratic(a, b, c):
+    """
+    Solve the quadratic equation returning
+    (root+, root-) even if they are the same
+    """
+    top_left = -b
+    discriminant = b * b - 4 * a * c
+    bottom = 2 * a
+
+    sol_plus = (top_left + cmath.sqrt(discriminant)) / bottom
+    sol_minus = (top_left - cmath.sqrt(discriminant)) / bottom
+    return (sol_plus, sol_minus)
+
+def grandmas_recipe(trace_a, trace_b, plus_root=True):
+    """
+    Grandma's Special Parabolic Commutator Groups
+    From Indra's Pearls p. 229
+
+    This formula is magical, I don't quite understand it
+    """
+    # make sure we have complex numbers
+    trace_a = complex(trace_a)
+    trace_b = complex(trace_b)
+
+    # 1 solve for Tr ab
+    # t_ab = x in the equation
+    # x^2 - Ta * Tb * x + Ta^2 + Tb^2 = 0
+    plus, minus = solve_quadratic(
+        1, -trace_a * trace_b, trace_a ** 2 + trace_b ** 2)
+
+    # Let the caller decide which root to use. Sometimes this matters
+    # when Tab is complex
+    trace_ab = plus if plus_root else minus
+
+    # Compute z0
+    z0_top = (trace_ab - 2) * trace_b
+    z0_bottom = trace_b * trace_ab - 2 * trace_a + 2j * trace_ab
+    z0 = z0_top / z0_bottom
+    print(z0)
+
+    # Compute the coefficients of a
+    A = trace_a / 2
+    B = (trace_a * trace_ab - 2 * trace_b + 4j) / ((2 * trace_ab + 4) * z0)
+    C = (trace_a * trace_ab - 2 * trace_b - 4j) * z0 / (2 * trace_ab - 4)
+    D = A
+    a = Mobius(A, B, C, D)
+
+    # Compute the coeffients of b
+    A = (trace_b - 2j) / 2
+    B = trace_b / 2
+    C = B
+    D = (trace_b + 2j) / 2
+    b = Mobius(A, B, C, D)
+
+    print(a)
+    print(b)
+
+    # Finally, return it as a group
+    return make_group(a, b)
+
+
 
 # The Glowing Gasket of Chapter 7 fame
 apollonian_gasket = make_group(
--- main.py
+++ main.py
@@ -6,20 +6,9 @@ import group_recipes
 from flame import Flame, FlamePack
 
 def main():
-    # unit circle
-    C = Cline.from_circle(1, 1)
-
-    # Transform to the imaginary line
-    K = mobius_recipes.cayley_map
-    C2 = C.transform(K)
-
-    print(C)
-    print(K)
-    print(C2)
-    print(C2.params)
-
     flames = [
-        Flame('ApollonianGasket', group_recipes.apollonian_gasket)
+        Flame('ApollonianGasket', group_recipes.apollonian_gasket),
+        Flame('GrandmasGasket', group_recipes.grandmas_recipe(2, 2))
     ]
     flame_pack = FlamePack('MobiusTest', flames)
     flame_pack.save('test.flame')
